e0319cd5ed3f2247868be88c6575a60841fad750
xuefengji <jxfvip@sina.com>
2020-07-20 21:25:00 +0800

    docs: update

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,34 +1,34 @@
 module.exports = [
 	{
-		text: '懵逼指南', 
+		text: '测试初级', 
 		//link: '/guide/'
 	}
-	//{
-	//	text: '面试宝典', 
-	//	link: '/baodian/',
-	//	items: [
-	//		{
-	//			text: '初级开发篇',
-	//			link: '/baodian/zero/'
-	//		},
-	//		{
-	//			text: '中高进阶篇',
-	//			link: '/baodian/high/'
-	//		}
-	//	]
-	//},
-	//{
-	//	text: '工具箱',
-	//	items: [
-	//		{
-	//			text: '在线编辑',
-	//			items: [
-	//				{
-	//					text: '阿里云',
-	//					link: 'https://www.aliyun.com/'
-	//				}
-	//			]
-	//		}
-	//	]
-	//}
+	{
+		text: '', 
+		link: '/baodian/',
+		items: [
+			{
+				text: '初级开发篇',
+				link: '/baodian/zero/'
+			},
+			{
+				text: '中高进阶篇',
+				link: '/baodian/high/'
+			}
+		]
+	},
+	{
+		text: '自动化测试',
+		items: [
+			{
+				text: '在线编辑',
+				items: [
+					{
+						text: '阿里云',
+						link: 'https://www.aliyun.com/'
+					}
+				]
+			}
+		]
+	}
 ]
