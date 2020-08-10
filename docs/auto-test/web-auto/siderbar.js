4219d782313835f784839fdbb9a92fbdfe2fe227
xuefengji <jxfvip@sina.com>
2020-08-10 21:38:32 +0800

    docs: add sidebar.js

---
+++ docs/auto-test/web-auto/siderbar.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: 'web-auto',
+	path: '/auto-test/web-auto/',
+    collapsable: false,
+    children: [
+			{
+				title: '一、web-basics',
+				path: '/auto-test/web-auto/web-basics.md',
+				collapsable: true
+			}
+    ]
+  }
+]
