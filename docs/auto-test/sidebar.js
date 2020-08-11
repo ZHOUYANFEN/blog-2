449717d8e8772107cd340d0297f6c5ed7af25af3
@xuefeng.ji <jxfvip@sina.com>
2020-08-11 16:23:52 +0800

    docs: update

--- docs/auto-test/sidebar.js
+++ docs/auto-test/sidebar.js
@@ -1,3 +1,3 @@
 module.exports = {
-	'/auto-test/web-auto': require('./web-auto/sidebar');
+	'/auto-test/web-auto': require('./web-auto/sidebar')
 }
--- docs/auto-test/web-auto/sidebar.js
+++ docs/auto-test/web-auto/sidebar.js
@@ -1,11 +1,11 @@
 module.exports = [
   {
-    title: 'web-auto',
-	path: '/auto-test/web-auto/',
+    title: '自动化测试',
+	path: '/auto-test/',
     collapsable: false,
     children: [
 			{
-				title: '一、web-basics',
+				title: '一、web 自动化',
 				path: '/auto-test/web-auto/web-basics.md',
 				collapsable: true
 			}
