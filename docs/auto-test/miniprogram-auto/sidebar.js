7860a7c3c54ecc2ab9c785ce6b8f175ef956a79c
xuefengji <jxfvip@sina.com>
2020-08-13 21:13:17 +0800

    docs: add file

---
+++ docs/auto-test/miniprogram-auto/sidebar.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: 'WEB 自动化测试',
+	path: '/auto-test/web-auto/',
+    collapsable: false,
+    children: [
+			{
+				title: '一、web 自动化',
+				path: '/auto-test/web-auto/web-basics.md',
+				collapsable: true
+			}
+    ]
+  }
+]
