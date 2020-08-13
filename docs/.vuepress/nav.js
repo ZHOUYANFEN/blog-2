cf591f8a80219c6e0714687c4b3a0bcefd370d9d
xuefengji <jxfvip@sina.com>
2020-08-13 21:10:23 +0800

    docs: update nav.js

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,12 +1,16 @@
 module.exports = [
 	{
 		text: '自动化测试',
-		link: '/auto-test/'
+		link: '/auto-test/',
 		items: [
 			{
 				text: 'web自动化测试',
 				link: '/auto-test/web-auto/'
-			}，
+			},
+			{
+				text: '小程序自动化测试',
+				link: '/auto-test/miniprogram-auto/'
+			}
 			
 		]
 	}
