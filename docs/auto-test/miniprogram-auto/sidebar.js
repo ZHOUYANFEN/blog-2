aa1402b948a61d23027bcc0a7c8d6424f072aa6b
xuefengji <jxfvip@sina.com>
2020-08-13 21:14:03 +0800

    docs: update

--- docs/auto-test/miniprogram-auto/sidebar.js
+++ docs/auto-test/miniprogram-auto/sidebar.js
@@ -1,12 +1,12 @@
 module.exports = [
   {
-    title: 'WEB 自动化测试',
-	path: '/auto-test/web-auto/',
+    title: '小程序自动化测试',
+	path: '/auto-test/miniprogram-auto/',
     collapsable: false,
     children: [
 			{
-				title: '一、web 自动化',
-				path: '/auto-test/web-auto/web-basics.md',
+				title: '一、小程序自动化基础',
+				path: '/auto-test/miniprogram-auto/miniprogram-basics.md',
 				collapsable: true
 			}
     ]
