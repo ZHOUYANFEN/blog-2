267f3282d6a4b1943de8af9c526e0a9b2cf6b1d7
xuefengji <xuefvip@163.com>
2020-08-16 17:43:32 +0800

    docs: formatter words

--- docs/auto-test/web-auto/sidebar.js
+++ docs/auto-test/web-auto/sidebar.js
@@ -1,11 +1,11 @@
 module.exports = [
   {
-    title: 'WEB 自动化测试',
+    title: 'Web 自动化测试',
 	path: '/auto-test/web-auto/',
     collapsable: false,
     children: [
 			{
-				title: '一、web 自动化基础',
+				title: '一、Web 自动化基础',
 				path: '/auto-test/web-auto/web-basics.md',
 				collapsable: true
 			}
