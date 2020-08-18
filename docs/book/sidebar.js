40261ec9b2e309119633690dc4150003e23658cd
xuefengji <xuefvip@163.com>
2020-08-18 21:25:01 +0800

    feat: add book/sidebar.js

---
+++ docs/book/sidebar.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: '读书笔记',
+	path: '/book/',
+    collapsable: false,
+    children: [
+			{
+				title: '数据思维',
+				path: '/book/data-thinking.md',
+				collapsable: true
+			}
+    ]
+  }
+]
