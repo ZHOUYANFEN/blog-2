5a4bd9587897d9bfc754122720c19a26b8b61908
xuefengji <xuefvip@163.com>
2020-08-18 21:24:12 +0800

    feat: update sidebar.js: add book/sidebar.js

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -2,5 +2,6 @@ module.exports = {
   '/auto-test/web-auto': require('../auto-test/web-auto/sidebar'),
   '/auto-test/miniprogram-auto': require('../auto-test/miniprogram-auto/sidebar'),
   '/product-and-operation/product': require('../product-and-operation/product/sidebar'),
-  '/product-and-operation/operation': require('../product-and-operation/operation/sidebar')
+  '/product-and-operation/operation': require('../product-and-operation/operation/sidebar')£¬
+  '/book/': require('../book/sidebar')
 }
