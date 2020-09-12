ba0ce42e1c987ca5821697e83fb41dbe7f3c5d0a
xuefengji <xuefvip@163.com>
2020-09-12 21:49:30 +0800

    feat: update: add others/sidebar.js

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -3,5 +3,6 @@ module.exports = {
   '/auto-test/miniprogram-auto': require('../auto-test/miniprogram-auto/sidebar'),
   '/product-and-operation/product': require('../product-and-operation/product/sidebar'),
   '/product-and-operation/operation': require('../product-and-operation/operation/sidebar'),
-  '/book': require('../book/sidebar')
+  '/book': require('../book/sidebar'),
+  'others': require('../others/sidebar')
 }
