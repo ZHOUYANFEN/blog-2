bb3a20860b05b4ccf607a69373bd6e9b96a8cc51
xuefengji <xuefvip@163.com>
2020-09-12 21:50:21 +0800

    feat: add sidebar.js

---
+++ docs/others/sidebar.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: 'others',
+	path: '/others/',
+    collapsable: false,
+    children: [
+    		{
+				title: '如何些总结',
+				path: '/others/summary.md',
+				collapsable: true
+			}
+    ]
+  }
+]
