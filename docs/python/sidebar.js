744d4803eb5d87682386ee196537f499483be14a
xuefengji <xuefvip@163.com>
2020-09-18 20:56:35 +0800

    feat: python add sidebar.js

---
+++ docs/python/sidebar.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: '其他',
+	path: '/python/',
+    collapsable: false,
+    children: [
+    		{
+				title: 'Python 基础',
+				path: '/python/base.md',
+				collapsable: true
+			}
+    ]
+  }
+]
