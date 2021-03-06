84861529202b518af1fa24ca75ee73cf26c7c025
xuefengji <xuefvip@163.com>
2021-03-06 10:19:47 +0800

    feat: update sidebar.js

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -4,6 +4,7 @@ module.exports = {
   '/product-and-operation/product': require('../product-and-operation/product/sidebar'),
   '/product-and-operation/operation': require('../product-and-operation/operation/sidebar'),
   '/book': require('../book/sidebar'),
-  '/others': require('../others/sidebar'),
+  '/others': require('../others/fund/sidebar'),
+  '/others': require('../others/others/sidebar'),
   '/python': require('../python/sidebar')
 }
