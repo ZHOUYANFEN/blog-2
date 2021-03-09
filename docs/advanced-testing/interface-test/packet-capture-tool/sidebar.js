f6f8d69ffe2c6898cd2b907b0c52ec51e8959de8
@xuefeng.ji <xuefvip@163.com>
2021-03-09 15:58:27 +0800

    docs: add file

---
+++ docs/advanced-testing/interface-test/packet-capture-tool/sidebar.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    title: 'Fiddler',
+	path: '//',
+    collapsable: false,
+    children: [
+			{
+				title: '一、基本界面介绍',
+				path: '/auto-test/miniprogram-auto/miniprogram-basics.md',
+				collapsable: true
+			}
+    ]
+  }
+]
