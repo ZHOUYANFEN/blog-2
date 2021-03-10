03e8268cde66b51c95a43424458eba8f739048c7
@xuefeng.ji <xuefvip@163.com>
2021-03-10 16:18:10 +0800

    feat: update garbage-collection

--- docs/python/sidebar.js
+++ docs/python/sidebar.js
@@ -8,6 +8,11 @@ module.exports = [
 				title: '数据类型',
 				path: '/python/base.md',
 				collapsable: true
+			}，
+			{
+				title: '垃圾回收机制',
+				path: '/python/garbage-collection.md',
+				collapsable: true
 			}
     ]
   }
