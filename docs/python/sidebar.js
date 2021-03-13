f9df89e0bab608daf0fb70349daf6a3dc8c45513
xuefengji <xuefvip@163.com>
2021-03-13 21:40:10 +0800

    fix: fix python/sidebar.js SyntaxError

--- docs/python/sidebar.js
+++ docs/python/sidebar.js
@@ -8,12 +8,12 @@ module.exports = [
 				title: '数据类型',
 				path: '/python/base.md',
 				collapsable: true
-			}，
+			},
 			{
 				title: '垃圾回收机制',
 				path: '/python/garbage-collection.md',
 				collapsable: true
-			}
+			},
     ]
   }
 ]
