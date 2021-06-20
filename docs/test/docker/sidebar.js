683bc7b4a93a00f8bcdd0236c99d79cb4599430e
xuefengji <xuefvip@163.com>
2021-06-20 22:23:03 +0800

    refactor: add sidebar.js

---
+++ docs/test/docker/sidebar.js
@@ -0,0 +1,35 @@
+module.exports = [
+  {
+    title: 'Docker',
+	path: '/test/docker/',
+    collapsable: false,
+    children: [
+    		{
+				title: 'docker核心技术',
+				path: '/test/docker/核心技术.md',
+				collapsable: true
+			}，
+    		{
+				title: 'docker安装',
+				path: '/test/docker/docker安装.md',
+				collapsable: true
+			}，
+			{
+				title: 'docker常用命令',
+				path: '/test/docker/docker常用命令.md',
+				collapsable: true
+			},
+			{
+				title: 'docker网络',
+				path: '/test/docker/docker网络.md',
+				collapsable: true
+			},
+			{
+				title: 'docker镜像制作',
+				path: '/test/docker/docker镜像制作.md',
+				collapsable: true
+			},
+			
+    ]
+
+]
