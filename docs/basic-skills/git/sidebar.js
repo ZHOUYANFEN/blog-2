f1442403283b4b894d5bb6aecfc52791ed106ced
xuefengji <xuefvip@163.com>
2021-08-14 16:45:14 +0800

    refactor: add sidebar.js

---
+++ docs/basic-skills/git/sidebar.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    title: 'git',
+	  path: '/basic-skill/git/',
+    collapsable: false,
+    children: [
+    		{
+				title: '问题',
+				path: '/basic-skill/git/problems.md',
+				collapsable: true
+			}
+			
+    ]
+}
+]
+
