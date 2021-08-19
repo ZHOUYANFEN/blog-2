0c1a202de00c4a2ff96bf8af60cdba8e37288ac3
@xuefeng.ji <xuefvip@163.com>
2021-08-19 17:18:43 +0800

    refactor：update catalog

--- docs/test/auto-test/sidebar.js
+++ docs/test/auto-test/sidebar.js
@@ -1,24 +1,71 @@
 module.exports = [
   {
     title: '自动化测试必备',
-	  path: '/test/auto-test/',
+	path: '/test/auto-test/',
     collapsable: false,
     children: [
 			{
-				title: '',
+				title: '自动化基础',
 				path: '/test/auto-test/web-basics.md',
 				collapsable: true
-			}
+			},
+			{
+				title: '数据驱动与页面对象模型',
+				path: '/test/auto-test/数据驱动与页面对象模型.md',
+				collapsable: true
+			}，
+			{
+				title: '页面对象模型',
+				path: '/test/auto-test/页面对象模型.md',
+				collapsable: true
+			},
+			{
+				title: '数据驱动',
+				path: '/test/auto-test/数据驱动.md',
+				collapsable: true
+			},
+			{
+				title: '提高GUI测试稳定性',
+				path: '/test/auto-test/提高GUI测试稳定性.md',
+				collapsable: true
+			},
+			{
+				title: '大型项目GUI自动化测试策略',
+				path: '/test/auto-test/大型项目GUI自动化测试设计策略.md',
+				collapsable: true
+			},
+			
+			
     ]
   },
   {
     title: 'selenium 框架',
-	  path: '/test/auto-test/selenium/',
+	path: '/test/auto-test/selenium/',
     collapsable: false,
     children: [
 			{
-				title: '一、自动化基础',
-				path: '/advanced-testing/auto-test/miniprogram-auto/miniprogram-basics.md',
+				title: 'Selenium原理',
+				path: '/test/auto-test/selenium/Selenium原理.md',
+				collapsable: true
+			},
+			{
+				title: '元素定位',
+				path: '/test/auto-test/selenium/selenium 定位方式.md',
+				collapsable: true
+			},
+			{
+				title: 'xpath语法',
+				path: '/test/auto-test/selenium/xpath语法.md',
+				collapsable: true
+			},
+			{
+				title: 'Selenium中xpath定位',
+				path: '/test/auto-test/selenium/selenium中xpath定位方式.md',
+				collapsable: true
+			},
+			{
+				title: 'CSS选择定位',
+				path: '/test/auto-test/selenium/css_selector.md',
 				collapsable: true
 			}
     ]
---
+++ docs/test/safety-test/sidebar.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    title: '自动化测试必备',
+	  path: '/test/auto-test/',
+    collapsable: false,
+    children: [
+			{
+				title: '',
+				path: '/test/auto-test/web-basics.md',
+				collapsable: true
+			}
+    ]
+  },
+  {
+    title: 'selenium 框架',
+	  path: '/test/auto-test/selenium/',
+    collapsable: false,
+    children: [
+			{
+				title: '一、自动化基础',
+				path: '/advanced-testing/auto-test/miniprogram-auto/miniprogram-basics.md',
+				collapsable: true
+			}
+    ]
+  }
+]
