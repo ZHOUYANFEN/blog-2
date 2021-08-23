9313aa5efff40229825952f27a12964ab24744d9
xuefengji <xuefvip@163.com>
2021-08-23 22:08:23 +0800

    refactor: Adjust the catalog

--- docs/languages/python/sidebar.js
+++ docs/languages/python/sidebar.js
@@ -1,89 +1,101 @@
 module.exports = [
   {
     title: 'Python',
-    path: '/languages/python/',
+    path: '/languages/python/python/',
     collapsable: false,
     children: [
     	{
         title: 'pip安装',
-        path: '/languages/python/pip安装与应用.md',
+        path: '/languages/python/python/pip安装与应用.md',
         collapsable: true
       	},
       	{
         title: '变量命名规范',
-        path: '/languages/python/命名规范.md',
+        path: '/languages/python/python/命名规范.md',
         collapsable: true
       	},
       	{
         title: '变量作用域',
-        path: '/languages/python/变量作用域.md',
+        path: '/languages/python/python/变量作用域.md',
         collapsable: true
       	},
       	{
         title: '数据类型',
-        path: '/languages/python/base.md',
+        path: '/languages/python/python/base.md',
         collapsable: true
       	},
       	{
         title: '循环',
-        path: '/languages/python/循环.md',
+        path: '/languages/python/python/循环.md',
         collapsable: true
       	},
       	{
         title: '函数',
-        path: '/languages/python/函数.md',
+        path: '/languages/python/python/函数.md',
         collapsable: true
       	},
       	{
         title: '继承',
-        path: '/languages/python/继承.md',
+        path: '/languages/python/python/继承.md',
         collapsable: true
       	},
       	{
         title: '特殊函数',
-        path: '/languages/python/magic-method.md',
+        path: '/languages/python/python/magic-method.md',
         collapsable: true
       	},
       	{
         title: '类与实例',
-        path: '/languages/python/类与实例.md',
+        path: '/languages/python/python/类与实例.md',
         collapsable: true
       	},
       	{
         title: '迭代器',
-        path: '/languages/python/迭代器.md',
+        path: '/languages/python/python/迭代器.md',
         collapsable: true
       	},
       	{
         title: '生成器',
-        path: '/languages/python/生成器.md',
+        path: '/languages/python/python/生成器.md',
         collapsable: true
       	},
       	{
         title: '推导式',
-        path: '/languages/python/推导式.md',
+        path: '/languages/python/python/推导式.md',
         collapsable: true
       	},
       	{
         title: '闭包',
-        path: '/languages/python/闭包.md',
+        path: '/languages/python/python/闭包.md',
         collapsable: true
       	},
       	{
         title: '装饰器',
-        path: '/languages/python/decorator.md',
+        path: '/languages/python/python/decorator.md',
         collapsable: true
       	},
       	{
         title: '垃圾回收机制',
-        path: '/languages/python/garbage-collection.md',
+        path: '/languages/python/python/garbage-collection.md',
         collapsable: true
       	},
       	{
         title: '进程与线程',
-        path: '/languages/python/multi-progress.md',
+        path: '/languages/python/python/multi-progress.md',
         collapsable: true
       	}
+    ]
+  },
+  {
+	title: 'Python',
+    path: '/languages/python/crawler',
+    collapsable: false,
+    children:[
+		{
+        title: '进程与线程',
+        path: '/languages/python/python/multi-progress.md',
+        collapsable: true
+      	},
     ]
   }
 ]
