db85710d636fda47b702016d3cd57da66e2fbedb
xuefengji <xuefvip@163.com>
2021-08-28 22:39:19 +0800

    refactor: remove md suffix

--- docs/basic-skills/docker/sidebar.js
+++ docs/basic-skills/docker/sidebar.js
@@ -6,27 +6,27 @@ module.exports = [
     children: [
     		{
 				title: 'docker核心技术',
-				path: '/basic-skills/docker/核心技术.md',
+				path: '/basic-skills/docker/核心技术',
 				collapsable: true
 			},
     		{
 				title: 'docker安装',
-				path: '/basic-skills/docker/docker安装.md',
+				path: '/basic-skills/docker/docker安装',
 				collapsable: true
 			},
 			{
 				title: 'docker常用命令',
-				path: '/basic-skills/docker/docker常用命令.md',
+				path: '/basic-skills/docker/docker常用命令',
 				collapsable: true
 			},
 			{
 				title: 'docker网络',
-				path: '/basic-skills/docker/docker网络.md',
+				path: '/basic-skills/docker/docker网络',
 				collapsable: true
 			},
 			{
 				title: 'docker镜像制作',
-				path: '/basic-skills/docker/docker镜像制作.md',
+				path: '/basic-skills/docker/docker镜像制作',
 				collapsable: true
 			},
 			
