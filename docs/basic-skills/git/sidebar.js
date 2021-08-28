11e89fd467d7b913f8c4e66975597eb656baeaee
xuefengji <xuefvip@163.com>
2021-08-28 22:40:09 +0800

    refactor: remove md suffix

--- docs/basic-skills/git/sidebar.js
+++ docs/basic-skills/git/sidebar.js
@@ -6,12 +6,12 @@ module.exports = [
     children: [
     		{
 				title: '常见问题',
-				path: '/basic-skills/git/problems.md',
+				path: '/basic-skills/git/problems',
 				collapsable: true
 			},
 			{
 				title: '生成秘钥',
-				path: '/basic-skills/git/生成秘钥.md',
+				path: '/basic-skills/git/生成秘钥',
 				collapsable: true
 			}
 			
