6a8dfeae1c6de3e54242b94dde5684a730104c88
xuefengji <xuefvip@163.com>
2021-08-28 22:41:58 +0800

    refactor: remove md suffix

--- docs/basic-skills/nginx/sidebar.js
+++ docs/basic-skills/nginx/sidebar.js
@@ -5,17 +5,17 @@ module.exports = [
     children: [
       {
         title: 'Nginx 安装',
-        path: '/basic-skills/nginx/installation.md',
+        path: '/basic-skills/nginx/installation',
         collapsable: true
       },
       {
         title: '基础配置',
-        path: '/basic-skills/nginx/基础配置.md',
+        path: '/basic-skills/nginx/基础配置',
         collapsable: true
       },
       {
         title: '配置文件',
-        path: '/basic-skills/nginx/配置文件.md',
+        path: '/basic-skills/nginx/配置文件',
         collapsable: true
       }
     ]
