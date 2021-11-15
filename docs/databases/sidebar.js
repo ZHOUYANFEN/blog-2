ff2ad0fe37cd4f62601858e5d4c1256933ec824d
xuefengji <xuefvip@163.com>
2021-11-15 21:23:48 +0800

    refactor: add sidebar.js

---
+++ docs/databases/sidebar.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+        title: 'MySql',
+        path: '/databases/mysql/',
+        collapsable: false,
+        children: [
+            {
+                title: '数据查询',
+                path: '/databases/mysql/data-query',
+                collapsable: true
+            },
+            {
+                title: '索引',
+                path: '/databases/mysql/index',
+                collapsable: true
+            },
+        ]
+    }
+]
