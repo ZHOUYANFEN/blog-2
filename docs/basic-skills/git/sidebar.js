8a6a923a1a0b01f58c540cc0157a240ce7e425d8
@xuefeng.ji <xuefvip@163.com>
2022-01-18 16:53:58 +0800

    docs: add installation steps

--- docs/basic-skills/git/sidebar.js
+++ docs/basic-skills/git/sidebar.js
@@ -4,16 +4,21 @@ module.exports = [
 	path: '/basic-skill/git/',
     collapsable: false,
     children: [
-    		{
-				title: '常见问题',
-				path: '/basic-skills/git/problems',
-				collapsable: true
-			},
 			{
 				title: '生成秘钥',
 				path: '/basic-skills/git/生成秘钥',
 				collapsable: true
-			}
+			},
+			{
+				title: '上传文件',
+				path: '/basic-skills/git/上传文件',
+				collapsable: true
+			},
+			{
+				title: '常见问题',
+				path: '/basic-skills/git/problems',
+				collapsable: true
+			},
 			
     ]
 }
