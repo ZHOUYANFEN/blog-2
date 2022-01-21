23e13d861a5062cb2b1dc3ea6e9b11b0161f6e4b
xuefengji <xuefvip@163.com>
2022-01-21 22:00:13 +0800

    refactor: add 三种状态机制

--- docs/test/interface-test/sidebar.js
+++ docs/test/interface-test/sidebar.js
@@ -9,6 +9,11 @@ module.exports = [
 			path: '/test/interface-test/Restful规范',
 			collapsable: true,
 		},
+		{
+			title: '三种状态机制',
+			path: '/test/interface-test/三种状态机制',
+			collapsable: true,
+		},
     ]
   },
   {
