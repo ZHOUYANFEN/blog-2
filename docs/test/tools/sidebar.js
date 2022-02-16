2abfe76ad881ed04267e6790838a4dcedd578bbd
xuefengji <xuefvip@163.com>
2022-02-16 20:06:16 +0800

    docs: modify path

--- docs/test/tools/sidebar.js
+++ docs/test/tools/sidebar.js
@@ -6,27 +6,27 @@ module.exports = [
     children: [
 			{
 				title: 'JMeter安装',
-				path: '/test/tools/Jmeter安装.md',
+				path: '/test/tools/Jmeter/Jmeter安装.md',
 				collapsable: true
 			},
 			{
 				title: 'JMeter目录介绍',
-				path: '/test/tools/Jmeter目录介绍.md',
+				path: '/test/tools/Jmeter/Jmeter目录介绍.md',
 				collapsable: true
 			},
 			{
 				title: 'JMeter配置文件',
-				path: '/test/tools/Jmeter配置文件.md',
+				path: '/test/tools/Jmeter/Jmeter配置文件.md',
 				collapsable: true
 			},
 			{
 				title: 'JMeter测试计划',
-				path: '/test/tools/测试计划详解.md',
+				path: '/test/tools/Jmeter/测试计划详解.md',
 				collapsable: true
 			},
 			{
 				title: 'JMeter测试计划参数介绍',
-				path: '/test/tools/测试计划参数详解.md',
+				path: '/test/tools/Jmeter/测试计划参数详解.md',
 				collapsable: true
 			},
     ]
